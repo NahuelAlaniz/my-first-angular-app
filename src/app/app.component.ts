db23fdc88149cc537d9efa9f1274418e44c9bdc3
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-11-16 16:59:31 -0300

    Se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'my-first-angular-app';
-}
+  template: `
+    Hello Universe
+  `,
+  styles:`
+    :host {
+      color:#a144eb;
+    }
+  `,
+    standalone: true,
+  })
+  export class AppComponent {}
