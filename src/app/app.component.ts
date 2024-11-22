5f04a88ab048fd0e98b2300a2be7e175347031ee
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-11-22 11:51:34 -0300

    Manejo de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.css'],
   template: `
-    <div [contentEditable]="isEditable"></div>
+    <section (mouseover)="onMouseOver()">
+      There´s a secret message for you, hover to reveal:
+      {{ message }}
+    </section>
   `,
 })
 export class AppComponent {
-  isEditable = true;
+  message = '';
+
+  onMouseOver() {
+    this.message = 'Way to go 🚀';
+  }
 }
