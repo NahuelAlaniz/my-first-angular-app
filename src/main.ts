1221d83e29cdad783602f9364dd219289eadc1e2
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-11-26 12:54:28 -0300

    Optimizando Imagenes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { UserComponent } from './app/user.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(UserComponent, appConfig)
   .catch((err) => console.error(err));
