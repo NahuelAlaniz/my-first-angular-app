c03bcc8db035e70095a7a0d57dbf354a5081b328
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-12-01 13:03:27 -0300

    Obteniendo el valor del formulario

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -4,16 +4,22 @@ import { FormsModule } from "@angular/forms";
 @Component({
     selector:'app-user',
     template:`
-      <p>Username:{{ username }}</p>
-      <p>{{ username }}'s favourite framework: {{ favoriteFramework}}</p>
+      <p>Username: {{ username }}</p>
+      <p>Framework: {{ favoriteFramework}}</p>
       <label for="framework">
         Favorite Framework:
         <input id = "framework" type = "text" [(ngModel)]="favoriteFramework">
       </label>
+      <button (click)="showFramework()">Show Framework</button>
     `,
     imports:[FormsModule],
 })
 export class UserComponent {
-  username = 'youngTech';
   favoriteFramework = '';
+  username = 'youngTech';
+
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
 }
+
