84bdd86b7d94df92b9863be7096ab7ef844040b6
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-12-04 10:09:17 -0300

    Creando un servicio inhectable

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,29 +1,10 @@
-import {Component} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
-import {ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component, inject } from "@angular/core";
+import { CarService } from "./car.service";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
-      <input type="text" formControlName="name" />
-      <input type="email" formControlName="email" />
-      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
-    </form>
-
-    <h2>Profile Form</h2>
-    <p>Name: {{ profileForm.value.name }}</p>
-    <p>Email: {{ profileForm.value.email }}</p>
-  `,
-  imports: [ReactiveFormsModule],
+  selector:'app-root',
+  template: '<p>{{ carService.getCars() }}</p>',
 })
 export class AppComponent {
-  profileForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-  });
-
-  handleSubmit() {
-    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
-  }
+  carService = inject(CarService);
 }
